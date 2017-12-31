5a6b22c7b4225b64dccbdaa1aad422022b69e3d0
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-12-31 12:27:35 +0100

    Pro ads updated

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://kaine.pl/projects/wp-plugins/wise-chat/wise-chat-donate
 Tags: chat, plugin, ajax, javascript, shortcode, social, widget, responsive, chat plugin, buddypress, buddypress chat, buddy, bp, chatbox, wise chat, wisechat, shoutbox, wordpress chat, online chat, social, chat software, webchat, free chat, community, visitor chat, im chat, pure chat, web chat, wordpress plugin, instant message, messaging, smiles, guestbook, themes, communication, contact
 Requires at least: 3.6
 Requires PHP: 5.2.0
-Tested up to: 4.8.2
+Tested up to: 4.9.1
 Stable tag: 2.6.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- src/admin/WiseChatProTab.php
+++ src/admin/WiseChatProTab.php
@@ -16,13 +16,18 @@ class WiseChatProTab extends WiseChatAbstractTab {
 				'<br />'.
 				'<h2>Boost user engagement, build a community, increase conversion!</h2>'.
 				'<h2 style="padding-top: 1px; font-size: 20px;">Try Wise Chat Pro plugin for WordPress and BuddyPress</h2>'.
+				'<br />'.
+				'<a class="button-secondary wcAdminButtonPro" target="_blank" href="https://kaine.pl/projects/wp-plugins/wise-chat-pro?source=settings-page" title="Check Wise Chat Pro">
+					Check Wise Chat <strong>Pro</strong>
+				</a>'.
 				'<br /><h3 style="font-size: 17px;">Wise Chat Pro features:</h3>'.
 				'<ul>'.
 				'<li>&#8226; All the features of Wise Chat free edition</li>'.
 				'<li>&#8226; Private one-to-one messages</li>'.
+				'<li>&#8226; Offline private messages</li>'.
 				'<li>&#8226; Avatars</li>'.
 				'<li>&#8226; Facebook-like chat mode</li>'.
-				'<li>&#8226; BuddyPress integration</li>'.
+				'<li>&#8226; BuddyPress integration: friends and group chats</li>'.
 				'<li>&#8226; Custom emoticons</li>'.
 				'<li>&#8226; Pending messages (fully moderated messages)</li>'.
 				'<li>&#8226; External authentication (via Facebook, Twitter or Google+)</li>'.
@@ -32,6 +37,10 @@ class WiseChatProTab extends WiseChatAbstractTab {
 				'<li>&#8226; Eternal license</li>'.
 				'<li>&#8226; Pay once, use forever</li>'.
 				'</ul>'.
+				'<a target="_blank" href="https://kaine.pl/projects/wp-plugins/wise-chat-pro?source=settings-page" title="Check Wise Chat Pro">
+					<img src="'.$this->options->getBaseDir().'/gfx/pro/wise-chat-pro-lead.png" />
+				</a>'.
+				'<br />'.
 				'<a class="button-secondary wcAdminButtonPro" target="_blank" href="https://kaine.pl/projects/wp-plugins/wise-chat-pro?source=settings-page" title="Check Wise Chat Pro">
 					Check Wise Chat <strong>Pro</strong>
 				</a>'
